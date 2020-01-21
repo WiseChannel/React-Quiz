6a87e6a92ced97a6062a42f0cf235b3997dd77b7
Ilya Wise <ilyaphotoshop17@gmail.com>
2020-01-21 16:05:19 +0200

    add some comp

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import classes from './Quiz.css'
+
+class Quiz extends Component{
+    render() {
+        return (
+            <div className={classes.Quiz}>
+                Wise
+            </div>
+        )
+    }
+}
+
+export default Quiz
